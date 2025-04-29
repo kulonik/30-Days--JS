4ac6cb20a8454a6effb64467058526004ea55e77
kulonik <annakhmv@gmail.com>
2025-04-29 22:28:16 +0500

    feat: find how many skills a user has'

---
+++ day8/level2/task.js
@@ -0,0 +1,85 @@
+const users = {
+  Alex: {
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: "asab@asab.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "Reac", "Redux"],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: "thomas@thomal.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+
+// 1.
+const copyUsers = Object.assign({}, users);
+console.log(copyUsers);
+const entries = Object.entries(copyUsers);
+console.log(entries);
+function getUsersInfo(array) {
+  let n = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][1]["skills"].length > n) {
+      n = array[i][1]["skills"].length;
+    }
+  }
+  return n;
+}
+console.log(getUsersInfo(entries));
+console.log(entries[0][1]["skills"].length);
