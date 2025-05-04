9c942d5a5327f4c548b6852b5816b891ff06d93f
kulonik <annakhmv@gmail.com>
2025-05-04 22:33:11 +0500

    feat: study topic about Set and Map and solve tasks at level 1

---
+++ day10/level1/task.js
@@ -0,0 +1,40 @@
+const countries = require("../../arrays/countries");
+
+// task 1
+const set = new Set();
+
+// task 2
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (const num of numbers) {
+  set.add(num);
+}
+console.log(set);
+
+// task 3
+console.log(set.delete(3));
+console.log(set);
+
+//  task 4
+set.clear();
+console.log(set);
+
+// task 5
+const array = ["JavaScript", "Java", "Python", "MongoDB", "React"];
+const stringSet = new Set(array);
+console.log(stringSet);
+
+// task 6
+const counts = [];
+const countryLength = {};
+const countriesSet = new Set(countries);
+for (const country of countriesSet) {
+  // console.log(country, country.length);
+  counts.push({ country: country, countryLength: country.length });
+}
+console.log(counts);
+
+const countriesMap = new Map();
+for (const { country, countryLength } of counts) {
+  countriesMap.set(country, countryLength);
+}
+console.log(countriesMap);
