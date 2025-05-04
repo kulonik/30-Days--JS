c81792a6d5dfc31e9735199e1eaf11e8e36d8843
kulonik <annakhmv@gmail.com>
2025-05-04 16:01:53 +0500

    feat: solve task 4 where write a function which returns an array of objects

--- day9/level2/task.js
+++ day9/level2/task.js
@@ -1,4 +1,5 @@
-const countries = ["Finland", "Sweden", "Denmark", "Norway", "IceLand"];
+const countries = require("../../arrays/countries");
+
 const names = ["Asabeneh", "Mathias", "Elias", "Brook"];
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const products = [
@@ -10,9 +11,58 @@ const products = [
   { product: "tea", price: "" },
 ];
 
+const alphebet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+// task 1
 console.log(
   products
     .map((item) => item.price)
     .filter((num) => typeof num === "number")
     .reduce((acc, cur) => acc + cur)
 );
+
+// task 3
+function categorizeCountries(array) {
+  return array.filter((item) => item.includes("land"));
+}
+console.log(categorizeCountries(countries));
+
+// task 4
+function getNumOfLetters(array1, array2) {
+  let arrayOfObjects = [];
+  for (let i = 0; i < array2.length; i++) {
+    let obj = {};
+    obj.letter = array2[i];
+    obj.number = array1.filter((item) => item.startsWith(array2[i])).length;
+    arrayOfObjects.push(obj);
+  }
+  return arrayOfObjects;
+}
+console.log(getNumOfLetters(countries, alphebet));
