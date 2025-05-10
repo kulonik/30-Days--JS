9d67bbda4a85f7fe302b83856ff12de065fd1df8
kulonik <annakhmv@gmail.com>
2025-05-10 11:26:20 +0500

    feat: solve task 2 of level 1

--- day12/level1/task.js
+++ day12/level1/task.js
@@ -14,3 +14,19 @@ function getWholeSum(array) {
   return sum;
 }
 console.log(getWholeSum(matches));
+
+// task 2
+const points = ["-1", "-2", "-4", "-3", "-1", "0", "4", "8"];
+const sortedPoints = points.sort((a, b) => a - b).join(" ");
+console.log(sortedPoints);
+
+const firstPattern = /\d$/g;
+const secondPattern = /^\-?\d/g;
+const match1 = sortedPoints.match(firstPattern);
+const match2 = sortedPoints.match(secondPattern);
+console.log(match1);
+console.log(match2);
+function getSumOfArrays(a, b) {
+  return Number(a) - Number(b);
+}
+console.log(getSumOfArrays(match1, match2));
