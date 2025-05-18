87eb7511cc3b084cc45436b326275c06c51058bd
kulonik <annakhmv@gmail.com>
2025-05-18 20:13:55 +0500

    feat: study task 2 of level 3

--- day18/level3/task2.js
+++ day18/level3/task2.js
@@ -4,12 +4,13 @@ const fetchData = async () => {
   try {
     const response = await fetch(countriesAPI);
     const countries = await response.json();
+    console.log(countries);
     let array = [];
     for (const country of countries) {
       array.push([country.name, country.area]);
     }
     const largestCountries = array.sort((a, b) => {
-      return b[1] - a[1];
+      return b[b.length - 1] - a[a.length - 1];
     });
     console.log(largestCountries.slice(0, 10));
   } catch (err) {
