d4f253d73f474a56dcacc1f9a5d3ef1250f6b0b9
kulonik <annakhmv@gmail.com>
2025-05-23 22:47:14 +0500

    feat: add numbers in blocks

--- day22/level1/script.js
+++ day22/level1/script.js
@@ -1,22 +1,19 @@
 const div = document.querySelector("div");
 div.style.cssText =
-  "margin: 50px auto; width: 1000px; height: 800px; display: flex; flex-direction: column; flex-wrap: wrap; gap: 1px; align-content: center";
-
-function addCode() {
-  div.innerHTML += `<div class="block"></div>`;
-}
-
-function getBlock() {
-  for (let i = 0; i <= 102; i++) {
-    addCode();
-  }
-}
-
-getBlock(addCode());
+  "margin: 50px auto; height: 800px; display: flex; flex-direction: column; flex-wrap: wrap; gap: 2px; align-content: center";
 
 let blocks = document.getElementsByClassName("block");
+
+function getBlock() {
+  for (let i = 0; i <= 101; i++) {
+    div.innerHTML += `<div class="block">${i}</div>`;
+  }
+}
+getBlock();
+
 const setOfNumbers = new Set(blocks);
 for (block of setOfNumbers) {
-  block.style.cssText = "background: grey; width: 80px; height: 60px";
+  block.style.cssText =
+    "background: grey; width: 90px; height: 45px; color: white; text-align: center; font-size: 24px; line-height: 40px";
 }
 console.log(setOfNumbers);
