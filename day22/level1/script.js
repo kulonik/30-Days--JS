ff9b929a2d1b4aaa549fe5768a6de989d34070aa
kulonik <annakhmv@gmail.com>
2025-05-24 21:46:42 +0500

    feat: add green and yellow color to blocks with even and odd numbers

--- day22/level1/script.js
+++ day22/level1/script.js
@@ -1,6 +1,6 @@
 const div = document.querySelector("div");
 div.style.cssText =
-  "margin: 50px auto; height: 800px; display: flex; flex-direction: column; flex-wrap: wrap; gap: 2px; align-content: center";
+  "margin: 50px auto; width: 600px; display: flex; flex-direction: row; flex-wrap: wrap; gap: 2px; align-content: center";
 
 let blocks = document.getElementsByClassName("block");
 
@@ -12,8 +12,24 @@ function getBlock() {
 getBlock();
 
 const setOfNumbers = new Set(blocks);
-for (block of setOfNumbers) {
+for (const block of setOfNumbers) {
   block.style.cssText =
     "background: grey; width: 90px; height: 45px; color: white; text-align: center; font-size: 24px; line-height: 40px";
 }
-console.log(setOfNumbers);
+console.log(blocks[0]);
+
+function setColor(array) {
+  let n = array.length;
+  for (let i = 0; i < n; i++) {
+    if (i == 2 && i == 3) {
+      array[i].style.background = "red";
+    }
+    if (i % 2 === 0) {
+      array[i].style.background = "green";
+    }
+    if (i % 2 !== 0) {
+      array[i].style.background = "#F6BE00";
+    }
+  }
+}
+setColor(blocks);
