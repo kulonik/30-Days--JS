91a06a3a973c2c9f104e53f3d3f4fcf54bfc02c8
kulonik <annakhmv@gmail.com>
2025-05-25 08:47:00 +0500

    feat: solve task of level 1

--- day22/level1/script.js
+++ day22/level1/script.js
@@ -1,4 +1,7 @@
 const div = document.querySelector("div");
+const h1 = document.querySelector("h1");
+
+h1.style.cssText = "text-align: center; margin-top: 20px; font-family: Courier";
 div.style.cssText =
   "margin: 50px auto; width: 600px; display: flex; flex-direction: row; flex-wrap: wrap; gap: 2px; align-content: center";
 
@@ -16,19 +19,44 @@ for (const block of setOfNumbers) {
   block.style.cssText =
     "background: grey; width: 90px; height: 45px; color: white; text-align: center; font-size: 24px; line-height: 40px";
 }
-console.log(blocks[0]);
 
 function setColor(array) {
   let n = array.length;
   for (let i = 0; i < n; i++) {
-    if (i == 2 && i == 3) {
-      array[i].style.background = "red";
-    }
-    if (i % 2 === 0) {
+    if (i % 2 === 0 || i === 0) {
       array[i].style.background = "green";
     }
     if (i % 2 !== 0) {
       array[i].style.background = "#F6BE00";
+    }
+    if (
+      (i > 1 && i < 4) ||
+      i === 5 ||
+      i === 7 ||
+      i === 11 ||
+      i === 13 ||
+      i === 17 ||
+      i === 19 ||
+      i === 23 ||
+      i === 29 ||
+      i === 31 ||
+      i === 37 ||
+      i === 41 ||
+      i === 43 ||
+      i === 47 ||
+      i === 53 ||
+      i === 59 ||
+      i === 61 ||
+      i === 67 ||
+      i === 71 ||
+      i === 73 ||
+      i === 79 ||
+      i === 83 ||
+      i === 89 ||
+      i === 97 ||
+      i === 101
+    ) {
+      array[i].style.background = "red";
     }
   }
 }
