f801289caca944ba186e2d053b984846f3a072c4
kulonik <annakhmv@gmail.com>
2025-05-28 21:56:21 +0500

    feat: add html and js file for ex. 1

---
+++ day23/ex1/script.js
@@ -0,0 +1,5 @@
+const h1 = document.querySelector("h1");
+const form = document.querySelector("form");
+const p = document.querySelector("p");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
