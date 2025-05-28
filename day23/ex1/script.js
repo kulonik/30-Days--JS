2c67dfe38053a6a7e116d159ccb7d235b15cb7d5
kulonik <annakhmv@gmail.com>
2025-05-28 22:14:00 +0500

    feat: add style to h1, input and button with JS

--- day23/ex1/script.js
+++ day23/ex1/script.js
@@ -3,3 +3,14 @@ const form = document.querySelector("form");
 const p = document.querySelector("p");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
+
+h1.style.cssText =
+  "font-family: courier; color:#24a724; text-align: center; margin-top: 30px";
+
+form.style.cssText =
+  "display: flex; flex-wrap: nowrap; width: 60%; justify-content: center; margin: 50px auto; gap: 10px";
+
+input.style.cssText = "border: 1px solid #24a724";
+
+button.style.cssText =
+  "border: solid 1px #24a724; background: #24a724; color: #fff; font-family: courier; font-size: 12px";
