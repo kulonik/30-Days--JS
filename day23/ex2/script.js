0aaff87ce324320714fa166c62c7c10a87a501d6
kulonik <annakhmv@gmail.com>
2025-06-01 11:02:25 +0500

    feat: add div with content and style it in js

---
+++ day23/ex2/script.js
@@ -0,0 +1,5 @@
+const div = document.querySelector("div");
+console.log(div);
+
+div.style.cssText =
+  "width: 350px; height: 40px; border: 1px solid rgb(238, 235, 235); box-shadow: 4px 4px 8px rgba(197, 189, 189, 0.3); margin: 150px auto; text-align: center; padding-top: 15px; font-size: 20px; font-weight: bold";
