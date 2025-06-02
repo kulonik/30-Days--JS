817807b1cba6b23f75aa8dc1f58d2cf75b318321
kulonik <annakhmv@gmail.com>
2025-06-02 09:26:58 +0500

    feat: add eventListener for keypress

--- day23/ex2/script.js
+++ day23/ex2/script.js
@@ -1,5 +1,11 @@
-const div = document.querySelector("div");
-console.log(div);
+const pressedKey = document.querySelector("#pressed-key");
+const key = document.querySelector("#key-code");
 
-div.style.cssText =
-  "width: 350px; height: 40px; border: 1px solid rgb(238, 235, 235); box-shadow: 4px 4px 8px rgba(197, 189, 189, 0.3); margin: 150px auto; text-align: center; padding-top: 15px; font-size: 20px; font-weight: bold";
+pressedKey.style.cssText =
+  "width: 350px; height: 40px; border: 1px solid rgb(238, 235, 235); box-shadow: 4px 4px 8px rgba(197, 189, 189, 0.3); margin: 150px auto 50px; text-align: center; padding-top: 15px; color: rgb(75, 63, 63); font-size: 24px; font-weight: bold; font-family: sans-serif; text-shadow: 4px 4px 2px rgba(197, 189, 189, 0.3)";
+
+document.body.addEventListener("keypress", (e) => {
+  key.textContent = e.keyCode;
+  key.style.cssText =
+    "width: 110px; height: 120px; margin: 0 auto; border: 1px solid rgb(238, 235, 235); box-shadow: 4px 4px 8px rgba(197, 189, 189, 0.3); color: rgb(38, 201, 16); font-weight: bold; font-size: 60px; text-shadow: 4px 4px 2px rgb(197, 189, 189, 0.3); text-align: center; padding-top: 30px";
+});
