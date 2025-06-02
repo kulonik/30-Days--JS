1a67b35a194a1fb60d16a6669c3fd44ebdd19369
kulonik <annakhmv@gmail.com>
2025-06-02 09:38:06 +0500

    feat: add innerHTML for pressedkey

--- day23/ex2/script.js
+++ day23/ex2/script.js
@@ -8,4 +8,6 @@ document.body.addEventListener("keypress", (e) => {
   key.textContent = e.keyCode;
   key.style.cssText =
     "width: 110px; height: 120px; margin: 0 auto; border: 1px solid rgb(238, 235, 235); box-shadow: 4px 4px 8px rgba(197, 189, 189, 0.3); color: rgb(38, 201, 16); font-weight: bold; font-size: 60px; text-shadow: 4px 4px 2px rgb(197, 189, 189, 0.3); text-align: center; padding-top: 30px";
+  let char = String.fromCharCode(e.keyCode);
+  pressedKey.innerHTML = `You pressed ${char}`;
 });
