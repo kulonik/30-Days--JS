c2e471ad8a0fad16d46180fefac9ff54f2339eb1
kulonik <annakhmv@gmail.com>
2025-06-03 21:17:07 +0500

    feat: add style to wrapper in js file

---
+++ day24/script.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector("h1");
+const wrapper = document.querySelector(".wrapper");
+
+wrapper.style.cssText =
+  "position: fixed; width: 100%; height: 100%; left: 0; top: 0; background: url(https://gifdb.com/images/high/night-sky-twinkling-stars-r1e0ty5782llkk0c.webp) no-repeat; background-size: cover";
+
+h1.style.cssText = "text-align: center; color: white;";
