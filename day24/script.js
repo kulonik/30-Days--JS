9b7e8408c357cb25a099eed8153ebac4c2b96dae
kulonik <annakhmv@gmail.com>
2025-06-06 11:23:56 +0500

    feat: add block Mass required and style it

--- day24/script.js
+++ day24/script.js
@@ -4,6 +4,8 @@ const form = document.querySelector("form");
 const mass = document.querySelector("#mass");
 const planet = document.querySelector("#planet");
 const button = document.querySelector("button");
+const content = document.querySelector("#content");
+const contentDesc = document.querySelector("#content-desc");
 
 wrapper.style.cssText =
   "position: fixed; width: 100%; height: 100%; left: 0; top: 0; background: url(https://gifdb.com/images/high/night-sky-twinkling-stars-r1e0ty5782llkk0c.webp) no-repeat; background-size: cover";
@@ -11,7 +13,7 @@ wrapper.style.cssText =
 h1.style.cssText =
   "text-align: center; color: white; margin-top: 50px; font-family: Arial";
 
-form.style.cssText = "margin: 80px auto; width: 450px";
+form.style.cssText = "margin: 80px auto 50px; width: 450px";
 
 mass.style.cssText = "width: 150px; height: 25px; font-size: 16px";
 
@@ -19,3 +21,9 @@ planet.style.cssText = "width: 120px; height: 30px; font-size: 16px";
 
 button.style.cssText =
   "width: 150px; height: 30px; font-size: 16px; background:rgba(157, 161, 161, 0.7); color: #ffffff";
+
+content.style.cssText =
+  "width: 80%; height: auto; padding: 50px; background: rgba(54, 56, 56, 0.4); margin: 0 auto";
+
+contentDesc.style.cssText =
+  "margin: 15px auto; width: 30%; padding: 20px; background: rgba(54, 56, 56, 0.4); text-align: center; color: #ffffff; font-family: Arial; font-size: 20px";
