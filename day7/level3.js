1a6ef7788781c642aa24a223f9c0e92ac153e952
kulonik <annakhmv@gmail.com>
2025-06-07 22:44:36 +0500

    feat: solve tasks 10 and 11 of level 3 at day 7

--- day7/level3.js
+++ day7/level3.js
@@ -2,4 +2,24 @@
 function factorial(n) {
   return n != 1 ? n * factorial(n - 1) : 1;
 }
-console.log(factorial(15));
+console.log(factorial(5));
+
+// 10. write function isEmpty
+function isEmpty(item) {
+  if (Boolean(item) == false) {
+    return "It is empty";
+  }
+  return "It is not empty";
+}
+console.log(isEmpty(0));
+
+// 11. call your function sum, it takes any number or arguments and it returns the sum.
+
+function sum(...args) {
+  let sum = 0;
+  for (const arg of args) {
+    sum += arg;
+  }
+  return sum;
+}
+console.log(sum(15, 25, 35, 50, 9, 4));
