51ffb6e2bb8e08ce1fb6fe1de3e6f07504c11e48
kulonik <annakhmv@gmail.com>
2025-06-12 22:03:23 +0500

    feat: add changes with lower and upper case in the name in task 2 of level 3 at day 8

--- day8/level3/task2.js
+++ day8/level3/task2.js
@@ -2,7 +2,9 @@ const users = require("./arrays");
 const prompt = require("prompt-sync")({ sigint: true });
 
 let yourName = prompt("Enter your name");
-
+let yourChangedName =
+  yourName.slice(0, 1).toUpperCase() + yourName.slice(1).toLowerCase();
+// a. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
 function signUp(array, name) {
   let newArr = [];
   for (let i = 0; i < array.length; i++) {
@@ -16,4 +18,13 @@ function signUp(array, name) {
   }
   return array;
 }
-console.log(signUp(users, yourName));
+console.log(signUp(users, yourChangedName));
+
+// b. Create a function called signIn which allows user to sign in to the application.
+let email = prompt("Enter your email");
+function signIn(array, user) {
+  array[array.length - 1].email = user;
+  return array;
+}
+signIn(users, email);
+console.log(signIn(users, email));
